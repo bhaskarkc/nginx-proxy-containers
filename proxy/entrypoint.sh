f7ee6f9ae312e227c7e22d0edb2cf2a7bf469e5b
Bhaskar <xlinkerz@gmail.com>
2019-08-29 21:18:25 +1000

    Refac (#1)
    * User docker file for docker-compose.yml
    
    * Update entrypoint.
    
    * update servers.json

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -1,33 +1,25 @@
-#!/usr/bin/env bash
-
-update_nginx_wrkr_ps() {
-    # Tweak nginx to match the workers of cpu's
-    procs=$(grep -c processor </proc/cpuinfo)
-    sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
-}
+#!/usr/bin/env sh
 
 build_server_directives() {
     if ! [[ -f ./servers.json ]]; then
         echo "Proxy servers settings not found. Aboting!"
-        exit 1
+        exit 1;
     fi
 
-    # install jq
-    apk --update add jq
-
-    jq -r '.[] | keys[]' <./servers.json | while read server_container; do
-
+    jq -r '.[] | keys[]' < ./servers.json | while read server_container;
+    do
         # Host availability check.
-        if ping -c 1 "$server_container" &>/dev/null; then
-            echo "$server_container is available."
+        if ping -c 1 "$server_container" &> /dev/null
+        then
+            echo "$server_container is available, adding config for this host..."
         else
             echo "$server_container is not available, skipping this host..."
             continue
         fi
 
-        server_name=$(jq -r ".[].$server_container | .server_name" <./servers.json)
-        app_port=$(jq -r ".[].$server_container | select(.app_port !=null) | .app_port" <./servers.json)
-        proxy_port=$(jq -r ".[].$server_container | select(.proxy_port !=null) | .proxy_port" <./servers.json)
+        server_name=$(jq -r ".[].$server_container | .server_name" < ./servers.json )
+        app_port=$(jq -r ".[].$server_container | select(.app_port !=null) | .app_port" < ./servers.json )
+        proxy_port=$(jq -r ".[].$server_container | select(.proxy_port !=null) | .proxy_port" < ./servers.json )
         proxy_url="http://$server_container"
 
         server=$(
@@ -41,11 +33,15 @@ build_server_directives() {
                     proxy_redirect off;
                 }
             }' "$server_container" "$server_container" "${app_port:-80}" "${proxy_port:-80}" "$server_name" "$proxy_url"
-        )
-
-        echo "$server" >/etc/nginx/conf.d/"$server_container".conf
-
+        );
+        echo "$server" > /etc/nginx/conf.d/"$server_container".conf
     done
+}
+
+update_nginx_wrkr_ps() {
+    # Tweak nginx to match the workers of cpu's
+    procs=$(cat < /proc/cpuinfo | grep -c processor)
+    sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
 }
 
 build_server_directives
